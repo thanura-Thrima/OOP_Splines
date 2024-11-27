cf67f6f157c5bf80817d4a6872497930c72b8e08
raveen thrimawithana <raveenthrimawithana111@gmail.com>
2024-11-27 09:09:21 +0200

    Adding Surface Interface

---
+++ RationalBezier/ISurface.h
@@ -0,0 +1,14 @@
+#pragma once
+
+template<class T, class V>
+class ISurface
+{
+public:
+	ISurface() {};
+	virtual ~ISurface() = 0;
+
+	virtual V operator[](T u,T v) = 0;
+};
+
+template<class T, class V>
+ISurface<T, V>::~ISurface(){}
